05d6840138635b6f506563b356296c022f55b147
Earn1ll <coldsteelblr@gmail.com>
2021-10-08 22:48:59 +0300

    add container component

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -1,48 +1,39 @@
 import s from './MyPosts.module.css';
 import Post from "./Post/Post";
 import React from 'react';
-import {addPostActionCreator, updateNewPostTextActionCreator} from "../../../redux/profileReducer";
-
-
 
 
 const MyPosts = (props) => {
-
-
     let postsElements =
-        props.posts.map( p =>  <Post message={p.message} likes={p.likes}/>);
+        props.posts.map(p => <Post message={p.message} likes={p.likes}/>);
 
     let newPostElement = React.createRef();
 
-    let addPost = () => {
-
-        props.dispatch(addPostActionCreator());
-
+    let onAddPost = () => {
+        props.addPost();
     }
     let onPostChange = () => {
         let text = newPostElement.current.value;
-        //let action = {type:'UPDATE-NEW-POST-TEXT', newText: text};
-        let action = updateNewPostTextActionCreator(text);
-        props.dispatch(action);
+        props.updateNewPostText(text);
     }
 
-    return (
-        <div className={s.content}>
+        return (
+            <div className={s.content}>
 
-            <div>
-               <h3>My posts</h3>
+                <div>
+                    <h3>My posts</h3>
+                </div>
+                <div>
+                    <textarea onChange={onPostChange} ref={newPostElement} value={props.newPostText}/>
+                </div>
+                <div>
+                    <button onClick={onAddPost}>Add post</button>
+                </div>
+                <div className={s.posts}>
+                    {postsElements}
+                </div>
             </div>
-            <div>
-                <textarea onChange={onPostChange} ref={newPostElement} value={props.newPostText} />
-            </div>
-            <div>
-                <button onClick={ addPost }>Add post</button>
-            </div>
-            <div className={s.posts}>
-                {postsElements}
-            </div>
-        </div>
 
-    );
-}
+        );
+    }
 export default MyPosts;
--- src/components/Profile/profile.jsx
+++ src/components/Profile/profile.jsx
@@ -1,20 +1,14 @@
-import s from './profile.module.css';
-import MyPosts from "./MyPosts/MyPosts";
+
 import ProfileInfo from "./ProfileInfo/ProfileInfo";
+import MyPostsContainer from "./MyPosts/MyPostsContainer";
 
 const Profile = (props) => {
-
-
 
     return (
         <div>
             <ProfileInfo/>
-            <MyPosts
-                posts={props.profilePage.posts}
-                newPostText ={props.profilePage.newPostText}
-                dispatch={props.dispatch}/>
+            <MyPostsContainer store = {props.store} />
         </div>
-
     )
 }
 export default Profile;
