264312e0a983ee609941e6fc800a184d9ba89df0
Earn1ll <coldsteelblr@gmail.com>
2021-10-16 00:19:45 +0300

    add UsersPage (bll and ui) v.1.01

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -2,6 +2,34 @@ import React from "react";
 import s from './users.module.css';
 
 let Users = (props) => {
+    if(props.users.length === 0) {
+
+    props.setUsers([{
+            id: 1,
+            followed: true,
+            photoUrl:'https://cdn.iconscout.com/icon/free/png-256/laptop-user-1-1179329.png',
+            status: 'I am a superFrontendDeveloper! :))',
+            location: {city: 'Brest', country: 'Belarus'}
+        },
+        {
+            id: 2,
+            followed: false,
+            photoUrl:'https://cdn.iconscout.com/icon/free/png-256/laptop-user-1-1179329.png',
+            fullName: 'Vasiliy',
+            status: 'Working hard!',
+            location: {city: 'Cheboksary', country: 'Russia'}
+        },
+        {
+            id: 3,
+            followed: true,
+            photoUrl:'https://cdn.iconscout.com/icon/free/png-256/laptop-user-1-1179329.png',
+            fullName: 'Alexey',
+            status: 'Hello people!',
+            location: {city: 'Minsk', country: 'Belarus'}
+        }
+        ])
+    }
+
     return <div>
         {
             props.users.map(u => <div key={u.id}>
@@ -11,8 +39,8 @@ let Users = (props) => {
                     </div>
                     <div>
                         {u.followed
-                            ? <button onClick={() =>{props.follow(u.id)}}>Unfollow</button>
-                            :<button>Follow</button>}
+                            ? <button onClick={() =>{props.unfollow(u.id)}}>Unfollow</button>
+                            :<button onClick={() =>{props.follow(u.id)}}>Follow</button>}
                     </div>
                 </span>
                 <span>
