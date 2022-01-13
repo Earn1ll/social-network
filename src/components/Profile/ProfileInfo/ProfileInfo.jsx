3234a44b5ff6701d74d09d610c3181e1ef06120e
Earn1ll <coldsteelblr@gmail.com>
2022-01-13 20:12:54 +0300

    setState,local state for Profile status

--- src/components/Profile/ProfileInfo/ProfileInfo.jsx
+++ src/components/Profile/ProfileInfo/ProfileInfo.jsx
@@ -1,22 +1,23 @@
 import s from './ProfileInfo.module.css';
 import Preloader from "../../common/Preloader/Preloader";
 import React from "react";
-
+import  ProfileStatus from './ProfileStatus';
 
 const ProfileInfo = (props) => {
     if (!props.profile) {
         return <Preloader />
     }
-    debugger
     return (
         <div>
-            <div>
+              {/* <div>
                 <img
                     src='https://images.pexels.com/photos/248797/pexels-photo-248797.jpeg?auto=compress&cs=tinysrgb&h=350'/>
-            </div>
+            </div>    */}
             <div className={s.descriptionBlock}>
-                <img src={props.profile.photos.large} />
-                ava + description
+                <div className={s.avatar}>
+                    <img src={props.profile.photos.large} />
+                </div>
+                <ProfileStatus status = {"hi!"}/>
             </div>
         </div>
     )
